4bac92161455697867cad057d74bd43d77f47057
adenilsonvecchio-hash <adenilsonvecchio@gmail.com>
2026-09-08 11:00:46 -0300

    Add files via upload

--- src/data/sports.ts
+++ src/data/sports.ts
@@ -1,12 +1,12 @@
 export const SPORTS = [
-  { id: 'tenis', name: 'Tênis', emoji: '🎾', color: '#a4c51d', image: './sports/tenis-v68.png' },
-  { id: 'futsal', name: 'Futsal', emoji: '⚽', color: '#1675ef', image: './sports/futsal-v68.png' },
-  { id: 'futebol-campo', name: 'Futebol de Campo', emoji: '⚽', color: '#168c5b', image: './sports/futebol-campo-v68.png' },
-  { id: 'beach-tennis', name: 'Beach Tennis', emoji: '🏓', color: '#e49a17', image: './sports/beach-tennis-v68.png' },
-  { id: 'handebol', name: 'Handebol', emoji: '🔵', color: '#f06432', image: './sports/handebol-v68.png' },
-  { id: 'volei', name: 'Vôlei', emoji: '🏐', color: '#3479d0', image: './sports/volei-v68.png' },
-  { id: 'basquete', name: 'Basquete', emoji: '🏀', color: '#e75e12', image: './sports/basquete-v68.png' },
-  { id: 'peteca', name: 'Peteca', emoji: '🏸', color: '#8a43ed', image: './sports/peteca-v68.png' },
+  { id: 'tenis', name: 'Tênis', emoji: '🎾', color: '#929da5', image: './sports/tenis-v73.png' },
+  { id: 'futsal', name: 'Futsal', emoji: '⚽', color: '#929da5', image: './sports/futsal-v73.png' },
+  { id: 'futebol-campo', name: 'Futebol de Campo', emoji: '⚽', color: '#929da5', image: './sports/futebol-campo-v73.png' },
+  { id: 'beach-tennis', name: 'Beach Tennis', emoji: '🏓', color: '#929da5', image: './sports/beach-tennis-v73.png' },
+  { id: 'handebol', name: 'Handebol', emoji: '🔵', color: '#929da5', image: './sports/handebol-v73.png' },
+  { id: 'volei', name: 'Vôlei', emoji: '🏐', color: '#929da5', image: './sports/volei-v73.png' },
+  { id: 'basquete', name: 'Basquete', emoji: '🏀', color: '#929da5', image: './sports/basquete-v73.png' },
+  { id: 'peteca', name: 'Peteca', emoji: '🏸', color: '#929da5', image: './sports/peteca-v73.png' },
 ] as const;
 
 export type SportId = typeof SPORTS[number]['id'];
